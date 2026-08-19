388ae230db2764bee8b7715f2b1a6f20825abf43
Krishna <krishnanandalm10@gmail.com>
2026-08-19 22:52:36 +0530

    Recursions Problems solved and Day 14 as well

--- Day_13/recursion.py
+++ Day_13/recursion.py
@@ -39,11 +39,10 @@ def count_up(num):
     if num <= 0:
         return
     else:
-        count_up(num -1) #Recursive case where it decreases to first
-        print(num) # then calls itself
+        count_up(num -1) #Recursive case  go down first
+        print(num) # then print coming back up 
 count_up(5)
 
-print()
 
 #2 print Numbers from n to 1 recursively
 
@@ -55,7 +54,7 @@ def count_down(n):
         count_down(n-1) #calls itself with a smaller value
 count_down(5)
 
-print()
+
 
 
 #3 Print Only Even Numbers using Recursion 
@@ -68,7 +67,7 @@ def even_numbers(n):
 
 even_numbers(10)
 
-print()
+
 
 #4 Print only Odd Numbers using Recursion 
 
@@ -81,7 +80,6 @@ def odd_numbers(num):
 
 odd_numbers(10)
 
-print() ## to skip one line thats it 
 
 #5 print the sum of n no of digits
 
@@ -90,4 +88,52 @@ def sum_num(num):
         return 0
     else:
         return num + sum_num(num-1)
+    
 print(sum_num(5))
+
+#6 Print factorial of a Number Recursively
+
+def fact(num):
+    if num == 1:
+        return 1 
+    return num * fact(num -1)
+
+print(fact(5))
+
+#7 Calculate power of a number(X**n) using recursion 
+
+def power_num(x,n):
+    if n == 0:
+        return 1 
+    return x * power_num(x , n-1)
+
+print(power_num(2,7))
+
+#8 find the nth fibonacci number recursively
+
+def fibonacci(num):
+    if num == 0:
+        return 0
+    if num == 1: #TWO BASE CONDITIONS
+        return 1
+    return fibonacci(num-1) + fibonacci(num-2) #calling recursive case twice
+print(fibonacci(5))
+
+
+#9 Print Fibonacci Series up to n terms recursively
+def fibonacci_series(a, b, n):
+    if n <= 0:
+        return
+
+    print(a)
+    fibonacci_series(b, a + b, n - 1)
+
+fibonacci_series(0, 1, 7)
+
+#10  Find the sume of Digits of a number
+
+def Sum_of_digits(num):
+    if num == 0:
+        return 0
+    return (num %10) + Sum_of_digits(num//10)
+print(Sum_of_digits(1234))
---
+++ Day_14/DateTIme.py
@@ -0,0 +1,11 @@
+## Python got datetime Module to handle date and time
+
+import datetime
+print(dir(datetime)) ## with this command it lets u known what are some of the built in commands
+
+from datetime import datetime
+now = datetime.now()
+print(now)
+day = datetime.day
+month = datetime.month
+year = datetime.year
